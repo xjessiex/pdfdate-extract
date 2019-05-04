8c2a846c282281684b220b5ee14114c6a08133fc
Jessie Yang <xy77@duke.edu>
2019-05-04 02:57:43 -0400

    add testing and finish sorting

--- main.py
+++ main.py
@@ -4,7 +4,9 @@
 import os
 import PyPDF2 as p2
 import re
-# import calendar
+from datetime import datetime
+import pandas as pd
+
 
 class Datecheck:
     # get working directory
@@ -13,7 +15,7 @@ class Datecheck:
     # initiation to open pdf file
     def __init__(self):
         print("Processing pdf to find the latest updated page:")
-        self.pdffile = open("electric-tariff.pdf","rb")
+        self.pdffile = open("electric-tariff.pdf", "rb")
         self.pdfread = p2.PdfFileReader(self.pdffile)
 
     def docinfo(self):
@@ -21,30 +23,78 @@ class Datecheck:
         docinfo = self.pdfread.getDocumentInfo()
         # extract the date for latest edits
         lastdate = docinfo['/ModDate']
-        print("The latest edit was made on Day %s, Month %s, Year %s." % (lastdate[9:10], lastdate[7:8],lastdate[2:6]))
+        print("The latest edit was made on Day %s, Month %s, Year %s." % (lastdate[9:10], lastdate[7:8], lastdate[2:6]))
 
         # number of pages in total
         print("There are %s pages in total." % (self.pdfread.getNumPages()))
 
-    def extractdate(self):
-
+    def extractnsort(self):
+        print("Now begin the date extraction process:")
         # create loop to extract single page
-        # i = 0
-        #while i < self.pdfread.getNumPages():
-            #x = self.pdfread.getPage(i)
-            #page_content = x.extractText()
+        i = 1  # skip the cover page
+        datelist = []
+
+        while i < self.pdfread.getNumPages():
+            x = self.pdfread.getPage(i)
+            page_content = x.extractText()
+            page_content = ''.join(page_content.split())
+            # print(page_content)
+
+            # attempt to locate date string (!!need to make sure there are no other dates)
+            # example: 03/01/2014
+            try:
+                page_date = re.search('\d{2}\/\d{2}\/\d{4}', page_content).group(0)
+                page_date = datetime.strptime(str(page_date), "%m/%d/%Y").date()
+            except AttributeError:
+                page_date = "NaN"
+            # print(page_date)
+            datelist.append([i + 1, page_date])
+            i += 1
+
+        print("Extraction complete. Now preview the table sorted by pages.")
+        df = pd.DataFrame(datelist, columns=["Page", "Date"])
+        print(df[:5])
+
+        # drop pages that we fail to load
+        df.clean = df[df["Date"] != 'NaN']
+
+        page_failure = len(df)-len(df.clean)
+        print("%s pages are unreadable in total." % page_failure)
+
+        # sort loaded pages based on dates
+        df_sorted= df.clean.sort_values(by="Date", ascending=False)
+        print("After sorting the date, now preview the top updated pages.")
+        print(df_sorted[:5])
 
 
-        x = self.pdfread.getPage(495)
+    def singletest(self):
+        print("For testing:")
+        # create loop to extract single page
+        i = 6
+        datelist = []
+
+        x = self.pdfread.getPage(i)
         page_content = x.extractText()
-        page_date = re.search('Date:(.+?)', page_content).group(1)
-        # print(page_content)
+        page_content = ''.join(page_content.split())
+        print(page_content)
+
+        # attempt to locate date string (!!need to make sure there are no other dates)
+        # example: 03/01/2014
+        try:
+            page_date = re.search('\d{2}\/\d{2}\/\d{4}', page_content).group(0)
+            page_date = datetime.strptime(str(page_date), "%m/%d/%Y").date()
+        except AttributeError:
+            page_date = "error in extracting date"
         print(page_date)
+        datelist.append([i + 1, page_date])
+
+
 
 def main():
     test = Datecheck()
     test.docinfo()
-    test.extractdate()
+    test.extractnsort()
+    #test.singletest() # for testing only
 
 
 if __name__ == "__main__":
